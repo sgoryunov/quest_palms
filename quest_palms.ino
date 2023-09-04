71222dd7447c3d56431aaa44ad94e54a67132113
Semyon Goryunov <ficuszet@gmail.com>
2023-09-04 20:22:41 +0300

    fix parameters

--- quest_palms.ino
+++ quest_palms.ino
@@ -1,16 +1,16 @@
 
 # define SEN_PIN A0
-# define RELAY_PIN 11
+# define RELAY_PIN 12
 
-const short triggerThreshold = 800;// Lower is more sensitive;
+const short triggerThreshold = 500;// Lower is more sensitive;
 
-const short countNum = 3;
+const short countNum = 5;
 short countSum = 0;
 short count = 0;
 short currVal = 1000;
 
 void setup() {
-  Serial.begin(9600);
+//  Serial.begin(9600);
   pinMode(SEN_PIN, INPUT);
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH);
@@ -20,11 +20,11 @@ void setup() {
 void loop() {
   short value = analogRead(SEN_PIN);
   short avrVal = GetAverage(value);
-  Serial.print("triggerThreshold is ");
-  Serial.print(value);
-  Serial.print("\t");
-  Serial.print("average triggerThreshold is ");
-  Serial.println(avrVal);
+//  Serial.print("triggerThreshold is ");
+//  Serial.print(value);
+//  Serial.print("\t");
+//  Serial.print("average triggerThreshold is ");
+//  Serial.println(avrVal);
   digitalWrite(RELAY_PIN, (avrVal < triggerThreshold) ? LOW : HIGH);
 }
 
